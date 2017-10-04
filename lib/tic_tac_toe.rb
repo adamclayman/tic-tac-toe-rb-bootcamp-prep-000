352eddb890cb8ceca33168b8ef8241378a650a6c
Adam Clayman <adam@clayman.org>
2017-10-04 15:39:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,6 +39,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
 def move(board, index, player_mark)
   board[index] = player_mark
 end
