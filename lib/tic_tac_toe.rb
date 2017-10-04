dbf53eb015b139219409e6ec106182e6af15e47b
Adam Clayman <adam@clayman.org>
2017-10-04 15:36:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,10 +39,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
 def move(board, index, player_mark)
   board[index] = player_mark
 end
@@ -52,7 +48,7 @@ def valid_move?(board, index)
 end
 
 def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " " || board[index] == "")
+  !(board[index] == nil || board[index] == " " || board[index] == "")
 end
 
 def turn_count(board)
